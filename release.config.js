8d256634a891bb58373c9d6c608566f8dbb7cf92
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-04-22 21:06:58 +0200

    fix: forgot to change the branches

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  branches: ['main', 'next'],
+  branches: ['master', 'development'],
   plugins: [
     [
       '@semantic-release/commit-analyzer',
