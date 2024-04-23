857dc5485191093ec8f09fa5e6270a0cc6fbe98d
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-04-23 17:00:06 +0200

    breaking change: jopa

--- release.config.js
+++ release.config.js
@@ -2,53 +2,17 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  branches: ['master', 'development'],
+  branches: ['main'],
   plugins: [
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        preset: 'angular',
-        releaseRules: [
-          {
-            breaking: true,
-            release: 'major',
-          },
-          {
-            type: 'feat',
-            release: 'minor',
-          },
-          {
-            type: 'fix',
-            release: 'patch',
-          },
-          {
-            type: 'docs',
-            scope: 'README',
-            release: 'patch',
-          },
-          {
-            type: 'chore',
-            release: 'patch',
-          },
-        ],
-        parserOpts: {
-          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
-        },
-      },
-    ],
+    '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
-    ],
+    '@semantic-release/changelog',
     '@semantic-release/github',
     [
       '@semantic-release/git',
       {
         assets: ['package.json', 'CHANGELOG.md'],
-        message: 'Verion: ${nextRelease.version}',
+        message: 'chore(release): ${nextRelease.version}',
       },
     ],
   ],
